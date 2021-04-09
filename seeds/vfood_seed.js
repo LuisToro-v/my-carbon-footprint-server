1312747f2f0b7210b39e6f4e845130bd69a9ba9b
Luis <vidals824@yahoo.com>
2021-04-09 14:35:06 -0400

    Altered user_seed and

--- seeds/vfood_seed.js
+++ seeds/vfood_seed.js
@@ -1,9 +1,21 @@
 const foodData = require('../seed_data/food')
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('food').del()
-    .then(function () {
       // Inserts seed entries
-      return knex('food').insert(foodData);
-    });
+      let foodPromise =[];
+      foodData.forEach((ent)=>{
+            let user = ent.username;
+            foodPromise.push(createEntries(knex, ent,user)) 
+      });
+      return Promise.all(foodPromise);
 };
+
+const createEntries= (knex,ent,user) => {
+      return knex('users').where('username', user).first()
+      .then((item)=>{
+            return knex('food').insert({
+                  result_food_total:ent.result_food_total,
+                  time_input: ent.time_input,
+                  user_id: item.id
+            })
+      })
+}
