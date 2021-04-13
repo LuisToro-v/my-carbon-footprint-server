5f05a762eff4223d39508066d50e8d72d65e8abc
Luis <vidals824@yahoo.com>
2021-04-13 14:04:55 -0400

    Added delete and update routes for transportation table

--- controllers/transport.js
+++ controllers/transport.js
@@ -20,7 +20,7 @@ const transportTotal = async(req,res)=> {
     }
 }
 
-// Retrieve food items of a user by start and end time. 
+// Retrieve transport items of a user by start and end time. 
 const transportTimeFrame = async(req,res)=> {
     let start = req.body.start;
     let end = req.body.end;
@@ -45,8 +45,54 @@ const transportTimeFrame = async(req,res)=> {
         res.status(500).send(err)
     }
 }
+// Delete Tranport item by id and user_id
+const deleteTransport = async(req,res)=> {
+    let id = req.body.id;
+    let user_id = req.body.user_id;
+    if(!(Number.isInteger(id) && Number.isInteger(user_id))){
+        return res.status(400).json({
+            message: "Bad input",
+        });
+    }
+    try{
+        await db.none("DELETE FROM transportation WHERE id = $1 AND user_id = $2",[id,user_id]);
+        return res.status(200).json({
+           message:"Entry Deleted"
+        });
+    }
+    catch(err){
+        res.status(500).send(err)
+    }
+}
+
+// Updates the result_transport total and time_input.
+//id of item, user_id, and new result_transport total must be given
+const updateTransport =  async(req,res)=> {
+    let id = req.body.id;
+    let user_id = req.body.user_id;
+    let total = req.body.result_transport_total
+    if(!(Number.isInteger(id) && Number.isInteger(user_id) && Number.isInteger(total))){
+        return res.status(400).json({
+            message: "Bad input",
+        });
+    }
+    try{
+        await db.none("UPDATE transportation SET " + 
+                     "result_transport_total = $1, " + 
+                      "time_input = CURRENT_TIMESTAMP " +
+                      "where id = $2 AND user_id = $3",[total,id,user_id]);
+        return res.status(200).json({
+           message:"Entry updated"
+        });
+    }
+    catch(err){
+        res.status(500).send(err)
+    }
+}
 
 module.exports = {
     transportTotal,
-    transportTimeFrame
+    transportTimeFrame,
+    deleteTransport,
+    updateTransport
 }
--- routes/transportRoutes.js
+++ routes/transportRoutes.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
-const { Router } = require('express')
-const {transportTotal,transportTimeFrame} = require('../controllers/transport')
+const { Router } = require('express');
+const {transportTotal,transportTimeFrame, deleteTransport,updateTransport} = require('../controllers/transport');
 const router = Router()
 
 router.post('/logTransport', transportTotal);
-router.post('/timeFrame', transportTimeFrame)
+router.post('/timeFrame', transportTimeFrame);
+router.delete('/delete', deleteTransport);
+router.patch('/update', updateTransport);
 module.exports = router;
