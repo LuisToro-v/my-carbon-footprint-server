ec2026b615c802dccef2dc53a2b0aabbdbde4b25
Luis <vidals824@yahoo.com>
2021-04-21 11:14:35 -0400

    Deleted knex.js and removed

--- knex.js
+++
@@ -1,8 +0,0 @@
-const knexfile = require('./knexfile')
-const env = process.env.NODE_ENV;
-const knex = require('knex')({
-    client: knexfile.development.client,
-    connection: process.env.DATABASE_URL || knexfile.development.connection,
-  });
-
-  module.exports = knex;
